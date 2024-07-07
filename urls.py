3f80f05ed9a36e2a58546e68a388c918df0a74f9
Pooja Nagapurkar <147425607+poojan456@users.noreply.github.com>
2024-07-07 13:46:29 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,18 +1,22 @@
+"""Track URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path , include
-from BTS import views
+
 urlpatterns = [
-    path('', views.home,name='home'),
-    path('map', views.map,name='map'),
-    path('search', views.search,name='search'),
-    path('index', views.index, name='index'),
-    #path('fetch-route-data/', views.fetch_route_data, name='fetch_route_data'),
-    #path('form', views.form,name='form'),
-    path('about',views.about,name='about'),
-    #path('example',views.example,name='example'),
-    path('get_route/', views.get_route, name='get_route'),
-    path('check_departure/', views.check_departure, name='check_departure'),
-    path('update_status/', views.update_status, name='update_status'),
-    path('reset_status/', views.reset_status, name='reset_status'),
-    path('swap_source_destination/', views.swap_source_destination, name='swap_source_destination'),
+    path('admin/', admin.site.urls),
+    path('', include('BTS.urls'))
 ]
